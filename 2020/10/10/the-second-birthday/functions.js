32b57cfcc2139912c53996fd71cfd3911c9052eb
Hammerzer <stellar_lzu@163.com>
2023-02-27 20:23:29 +0800

    Site updated: 2023-02-27 20:23:17

--- 2020/10/10/the-second-birthday/functions.js
+++ 2020/10/10/the-second-birthday/functions.js
@@ -56,5 +56,5 @@ function timeElapse(date){
 		seconds = "0" + seconds;
 	}
 	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
-	$("#clock").html(result);
+	$("#myclock").html(result);
 }
