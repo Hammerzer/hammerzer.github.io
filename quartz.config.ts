46633c0b01a9f1ba2f5c2d1886b9a654ab7867c6
hammerzer <wanghx19319@163.com>
2026-02-08 13:59:53 +0800

    配置 GitHub Pages 部署

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "hammerzer.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
